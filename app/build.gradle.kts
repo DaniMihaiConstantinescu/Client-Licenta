231b7df75ec28a594e58ea6cf17a42d0feca601e
DaniConstantinescu <mihai.constantinescu02@e-uvt.ro>
2024-03-13 15:22:37 +0200

    Update build.gradle.kts
    * added dependencies for retrofit + gson

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,10 +76,16 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
+    // jetpack compose
     implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
 
+    // compose lifecycle
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+
+    // retrofit + gson
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.google.code.gson:gson:2.10.1")
 
     implementation("io.github.raamcosta.compose-destinations:core:1.1.2-beta")
     ksp("io.github.raamcosta.compose-destinations:ksp:1.1.2-beta")
