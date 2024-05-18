ae3eb634053d1988321959afc1cab4ca30650ab1
DaniMihaiConstantinescu <mihai.constantinescu02@e-uvt.ro>
2024-05-18 12:37:17 +0300

    Added google icon to login

--- app/src/main/java/com/example/testapp/ui/auth/SignInScreen.kt
+++ app/src/main/java/com/example/testapp/ui/auth/SignInScreen.kt
@@ -1,17 +1,27 @@
 package com.example.testapp.ui.auth
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.testapp.R
 import com.example.testapp.utils.auth.SignInState
 
 @Composable
@@ -33,8 +43,24 @@ fun SignInScreen(
             .padding(20.dp),
         contentAlignment = Alignment.Center
     ) {
-        Button(onClick = onSignInClick) {
-            Text(text = "Sign in with Google")
+        Button(
+            onClick = onSignInClick,
+            shape = RoundedCornerShape(12.dp) ,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.secondaryContainer
+            )
+            ) {
+            Image(
+                painter = painterResource(id = R.drawable.google_icon),
+                contentDescription = "Google Icon",
+                Modifier.size(48.dp)
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            Text(
+                text = "Sign in with Google",
+                color = Color.White,
+                fontSize = MaterialTheme.typography.titleMedium.fontSize
+            )
         }
     }
 
